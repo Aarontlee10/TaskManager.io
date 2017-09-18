3309ffd0b7c3e9338685d0fbc95c6425d8aa2e87
Aaron Lee <aarontlee10@berkeley.edu>
2017-09-17 17:49:07 -0700

    finished implementing the RESTful API, implementing the client now

--- routes/tasks.js
+++ routes/tasks.js
@@ -12,6 +12,7 @@ router.get('/tasks', function(req, res, next){
 		res.json(tasks);
 	});
 });
+
 //Get Single Tasks
 router.get('/task/:id', function(req, res, next){
 	db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, task) {
@@ -55,12 +56,25 @@ router.put('/task/:id', function(req, res, next){
 	var task = req.body;
 	var updTask = {};
 
-	db.tasks.remove({_id: mongojs.ObjectId(req.params.id)}, function(err, task) {
-		if(err){
-			res.send(err);
-		}
-		res.json(task);
+	if(task.isDone) {
+		updTask.isDone = task.isDone;
+	}
+	if(task.title) {
+		updTask.title = task.title;
+	}
+	if(!updTask){
+		res.status(400);
+		res.json({
+			"error":"Bad Data"
+		});
+	}else {
+		db.tasks.update({_id: mongojs.ObjectId(req.params.id)}, updTask, {}, function(err, task) {
+			if(err){
+				res.send(err);
+			}
+			res.json(task);
 	});
+	}
 });
 
 module.exports = router;
